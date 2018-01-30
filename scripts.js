5b3eb2dcc4c4ee9ce7674b50052d6e7511292832
q349wang <q349wang@edu.uwaterloo.ca>
2018-01-30 15:22:48 -0500

    Cleaning up animation scripts to use jQuery

--- scripts.js
+++ scripts.js
@@ -1,20 +1,11 @@
-function fadeIn(elem, speed, opac, target) { /*Fades in an array of elements at given speed in ms, from a starting opac to target*/
+function fade(elem, speed, target) { /*Fades  an page given speed in ms, to a target opac using jQuery*/
 
-	var id = setInterval(frame, speed);
-
-	function frame() {
-		if (opac >= target) {
-			clearInterval(id);
-		} else {
-			opac += 0.01;
-			for(i = 0; i< elem.length; i++)	{
-				elem[i].style.opacity = opac; 
-			}
-		}
-	}
+	$(elem).animate({
+		opacity: target
+	}, speed);
 }
-
-function fadeOut(elem, speed, opac, target) { /*Fades out an array of elements at given speed in ms, from a starting opac to target*/
+/* Depreciated
+function fadeOut(elem, speed, opac, target) { Fades out an array of elements at given speed in ms, from a starting opac to target using jQuery
 
 	var id = setInterval(frame, speed);
 
@@ -28,55 +19,20 @@ function fadeOut(elem, speed, opac, target) { /*Fades out an array of elements a
 			}
 		}
 	}
-}
+}*/
 
-function fadeOutPage(elem, speed, opac, target, next) { /*Fades out an page given speed in ms, from a starting opac to target and goes to the next page*/
+function fadeOutPage(elem, speed, target, next) { /*Fades  an page given speed in ms, to a target opac and goes to the next page using jQuery*/
 
-	var id = setInterval(frame, speed);
+	$(elem).animate({
+		opacity: target
+	}, speed, 
+	function() {window.location.href=next;});
 
-	function frame() {
-		if (opac <= target) {
-			clearInterval(id);
-			window.location.href=next;
-		} else {
-			opac -= 0.01;
-			for(i = 0; i< elem.length; i++)	{
-				elem[i].style.opacity = opac; 
-			}
-		}
-	}
 }
 
 
-function translateTop(elem, speed, x) { /*Moves an array of elements (top space) at given speed in ms, a given x*/
-
-	var initX = [];
-	var currX = []
-
-	for(i = 0; i< elem.length; i++)	{
-
-		initX.push(elem[i].style.top);
-		currX.push(elem[i].style.top);
-
-	}
-
-
-	var id = setInterval(frame, speed);
-
-	function frame() {
-		for(i = 0; i< elem.length; i++)	{
-
-			if (currX[i] == x + initX[i]) {
-				clearInterval(id);
-			} else if(currX < x + initX[i]) {
-				currX[i] += 0.1;
-			} else {
-				currX[i] -= 0.1;
-
-			}
-
-			elem[i].style.top = currX[i] + '%'; 
-
-		}
-	}
+function translateTop(elem, speed, x) { /*Moves an array of elements (top space) at given speed in ms, a given x percent using jQuery*/
+	$(elem).animate({
+		top: "+=" + x + "%"
+	}, speed);
 }
