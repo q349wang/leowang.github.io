3caeaa38a4285ac5d35c9f0c15d2323843796ae4
q349wang <q349wang@edu.uwaterloo.ca>
2018-09-19 15:01:15 -0400

    modified scrapper for github

--- scripts/quoteScrapper.py
+++ scripts/quoteScrapper.py
@@ -3,17 +3,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-r  = requests.get('https://www.brainyquote.com/topics/lobster?vm=l')
+r  = requests.get('https://github.com/q349wang')
 
 data = r.text
 
 soup = BeautifulSoup(data, 'html.parser')
 
-quotes = soup.find_all("a", class_="b-qt")
-authors = soup.find_all("a", class_="bq-aut")
+print(soup.prettify())
+#quotes = soup.find_all("a", class_="b-qt")
+#authors = soup.find_all("a", class_="bq-aut")
 
-for quote, author in zip(quotes, authors):
-    print(quote.get_text())
-    print(author.get_text())
-    print()
+#for quote, author in zip(quotes, authors):
+#    print(quote.get_text())
+#    print(author.get_text())
+#    print()
 
